027cbd7bdf926eea1838935e97d1550ac231ff96
Oliver Daniels-Koch <danielskoch.oliver@gmail.com>
2025-05-07 13:57:42 -0400

    24 hour training

--- train_job.sh
+++ train_job.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1               # Number of tasks
 #SBATCH --cpus-per-task=1        # Number of CPU cores per task
 #SBATCH --mem=1G                 # Memory required (1 GB in this example)
-#SBATCH --time=12:00:00          # Time limit (HH:MM:SS)
+#SBATCH --time=24:00:00          # Time limit (HH:MM:SS)
 
 module load conda/latest
 conda activate poker_mcts
