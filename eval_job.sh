d33321560706a82c2e513b5311f0e63835598d1f
Oliver Daniels-Koch <danielskoch.oliver@gmail.com>
2025-05-07 13:55:28 -0400

    rename and add eval job

---
+++ eval_job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=poker_mcts        # Job name
+#SBATCH --output=output_log/job_%j.out      # Output file (%j expands to jobID)
+#SBATCH --error=output_log/job_%j.err       # Error file (%j expands to jobID)
+#SBATCH --ntasks=1               # Number of tasks
+#SBATCH --cpus-per-task=1        # Number of CPU cores per task
+#SBATCH --mem=1G                 # Memory required (1 GB in this example)
+#SBATCH --time=12:00:00          # Time limit (HH:MM:SS)
+
+module load conda/latest
+conda activate poker_mcts
+
+exp_dir=$1
+
+python tournament_eval.py $exp_dir
