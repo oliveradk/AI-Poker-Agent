6337acb82096f8f1f3f3276468e6ada5272906c4
Oliver Daniels-Koch <danielskoch.oliver@gmail.com>
2025-05-07 12:24:24 -0400

    added job file

---
+++ job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=poker_mcts        # Job name
+#SBATCH --output=output_log/job_%j.out      # Output file (%j expands to jobID)
+#SBATCH --error=output_log/job_%j.err       # Error file (%j expands to jobID)
+#SBATCH --ntasks=1               # Number of tasks
+#SBATCH --cpus-per-task=1        # Number of CPU cores per task
+#SBATCH --mem=1G                 # Memory required (1 GB in this example)
+#SBATCH --time=12:00:00          # Time limit (HH:MM:SS)
+
+module load conda 
+conda activate poker_mcts
+
+cd AI-Poker-Agent
+
+python train.py
+
