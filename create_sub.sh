307936bfa6d34acf5c8f4159ec70aa44d0c6da8d
Oliver Daniels-Koch <danielskoch.oliver@gmail.com>
2025-05-08 15:26:08 -0400

    script for generating submission

---
+++ create_sub.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+out_dir=$1
+epoch=$2
+
+rm -rf submission
+mkdir submission
+cp custom_player.py submission/
+cp q_learn_player.py submission/
+cp $out_dir/$epoch/Q.npy submission/
+cp $out_dir/$epoch/N.npy submission/
+cp $out_dir/config.json submission/
