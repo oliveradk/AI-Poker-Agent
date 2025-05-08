6acd37abdef47b9868040d9cf3e0160b2ef170a6
Oliver Daniels-Koch <danielskoch.oliver@gmail.com>
2025-05-08 15:44:30 -0400

    add hand_eval and auto-zip

--- create_sub.sh
+++ create_sub.sh
@@ -7,6 +7,8 @@ rm -rf submission
 mkdir submission
 cp custom_player.py submission/
 cp q_learn_player.py submission/
+cp hand_eval.py submission/
 cp $out_dir/$epoch/Q.npy submission/
 cp $out_dir/$epoch/N.npy submission/
 cp $out_dir/config.json submission/
+zip -r submission.zip submission
